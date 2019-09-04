e937549a124ae50d188366be923618912c1099fb
Ninh <le.van.ninh@sun-asterisk.com>
2019-09-04 12:59:36 +0700

    Example project angular in detail

---
+++ example/src/app/component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Model } from './model/repository.model';
+
+@Component({
+  selector: 'app',
+  templateUrl: 'template.html'
+})
+
+export class ProductComponent {
+  model: Model = new Model();
+}
