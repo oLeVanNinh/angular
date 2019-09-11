ee099202f851022c1b149c5fe493de2d4b0a77ae
oLeVanNinh <ninhle.9984@gmail.com>
2019-09-11 09:37:37 +0700

    Provider service (#6)

---
+++ example/src/app/directives/valueDisplay.directive.ts
@@ -0,0 +1,14 @@
+import { Directive, InjectionToken, Inject, HostBinding, Host, Optional } from '@angular/core';
+export const VALUE_SERVICE = new InjectionToken('value_service');
+
+@Directive({
+  selector: '[paDisplayValue]'
+})
+
+export class PaDisplayValue {
+  constructor(@Inject(VALUE_SERVICE) @Host() @Optional() serviceValue: string) {
+    this.elementContent = serviceValue || "No value";
+  }
+
+  @HostBinding('textContent') elementContent: string;
+}
--- example/src/app/services/discount.service.ts
+++ example/src/app/services/discount.service.ts
@@ -1,9 +1,12 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
+import { LogService, LOG_SERVICE, LogLevel } from './log.service';
 
 @Injectable()
 
 export class DiscountService {
   private discountValue: number = 10;
+
+  constructor(private logger: LogService) { }
 
   public get discount(): number {
     return this.discountValue;
@@ -14,6 +17,7 @@ export class DiscountService {
   }
 
   public applyDiscount(price: number) {
+    this.logger.logInfoMessage(`Discount ${this.discount} applied to price: ${price}`);
     return Math.max(price - this.discountValue, 5)
   }
 }
