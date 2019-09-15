0ad9f06d062176898d168dcd455dd915edc882a7
oLeVanNinh <ninhle.9984@gmail.com>
2019-09-15 11:11:46 +0700

    Subscribe event using rxjs (#9)

--- exampleApp/src/app/app.module.ts
+++ exampleApp/src/app/app.module.ts
@@ -3,8 +3,8 @@ import { NgModule } from '@angular/core';
 // import { AppComponent } from './app.component';
 import { ModelModule } from './model/model.module';
 import { CoreModule } from './core/core.module';
-import { TableComponent } from './core/table.component';
-import { FormComponent } from './core/form.component';
+import { TableComponent } from './core/table/table.component';
+import { FormComponent } from './core/form/form.component';
 import { MessageModule } from './messages/message.module';
 import { MessageComponent } from './messages/message.component';
 
