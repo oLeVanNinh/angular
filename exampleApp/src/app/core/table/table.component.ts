26fbdbbc1f68b787ee46ad80c762f8fed78ee00f
oLeVanNinh <ninhle.9984@gmail.com>
2019-09-16 19:02:34 +0700

    Using children route (#12)

--- exampleApp/src/app/core/table/table.component.ts
+++ exampleApp/src/app/core/table/table.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { Product } from '../../model/product.model';
 import { Model } from '../../model/repository.model';
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'paTable',
@@ -8,14 +9,24 @@ import { Model } from '../../model/repository.model';
 })
 
 export class TableComponent {
-  constructor(private model: Model) {}
+  category: string = null;
+
+  constructor(private model: Model, activeRoute: ActivatedRoute) {
+    activeRoute.params.subscribe(params => {
+      this.category = params['category'] || null;
+    })
+  }
 
   getProduct(key: number): Product {
     return this.model.getProduct(key);
   }
 
   getProducts(): Product[] {
-    return this.model.getProducts();
+    return this.model.getProducts().filter(p => this.category == null || p.category == this.category);
+  }
+
+  get categories(): string[] {
+    return this.model.getProducts().map(p => p.category).filter((category, index, array) => array.indexOf(category) == index)
   }
 
   deleteProduct(key: number) {
